8be606dbcc75f050c6643f4d31849e645b6581eb
Trieu Huynh Pham Nhat <vikingtc4@gmail.com>
2023-09-14 15:47:37 +0700

    Update for assignment 7 part 1.

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -32,6 +32,51 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     /**
     * TODO: implement per description
     */
+
+    // Nothing in the circular buffer currently
+    if ((buffer->out_offs==buffer->in_offs) && !buffer->full)
+        return NULL;
+
+    // Case 1: out_offs < in_offs
+    if(buffer->out_offs < buffer->in_offs){
+        int temp_out_offs = buffer->out_offs;
+        while(temp_out_offs < buffer->in_offs){
+            if(char_offset < buffer->entry[temp_out_offs].size){
+                *entry_offset_byte_rtn = char_offset;
+                return &buffer->entry[temp_out_offs];
+            }
+            else{
+                char_offset -= buffer->entry[temp_out_offs].size;
+                temp_out_offs ++;
+            }
+        }
+    }
+    // Case 2: in_offs > out_offs
+    else{
+        int temp_out_offs = buffer->out_offs;
+        int temp_in_offs = buffer->in_offs;
+        int i;
+        while(temp_out_offs < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED){
+            if(char_offset < buffer->entry[temp_out_offs].size){
+                *entry_offset_byte_rtn = char_offset;
+                return &buffer->entry[temp_out_offs];
+            }
+            else{
+                char_offset -= buffer->entry[temp_out_offs].size;
+                temp_out_offs ++;
+            }
+        }
+        for(i = 0; i < temp_in_offs; i++){
+            if(char_offset < buffer->entry[i].size){
+                *entry_offset_byte_rtn = char_offset;
+                return &buffer->entry[i];
+            }
+            else{
+                char_offset -= buffer->entry[i].size;
+            }
+        }
+    }
+
     return NULL;
 }
 
@@ -47,6 +92,21 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description
     */
+
+    // Check if the buffer is full, and if so, advance out_offs to overwrite the oldest entry
+    if (buffer->full)
+    {
+        buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+
+    // Copy the new entry to the buffer at the current in_offs position
+    buffer->entry[buffer->in_offs] = *add_entry;
+    buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+    // Update the full flag
+    if(buffer->in_offs == buffer->out_offs)
+        buffer->full = true;
+
 }
 
 /**
